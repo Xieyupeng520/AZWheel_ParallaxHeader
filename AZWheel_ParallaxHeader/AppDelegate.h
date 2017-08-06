b5c619bd90517cf069bb3f20c3397d47d7e37109
Xieyupeng520 <806622085@qq.com>
2017-08-06 10:42:16 +0800

    Initial Commit

---
+++ AZWheel_ParallaxHeader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AZWheel_ParallaxHeader
+//
+//  Created by 阿曌 on 17/8/6.
+//  Copyright © 2017年 阿曌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AZWheel_ParallaxHeader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AZWheel_ParallaxHeader
+//
+//  Created by 阿曌 on 17/8/6.
+//  Copyright © 2017年 阿曌. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
